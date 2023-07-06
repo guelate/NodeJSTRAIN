981cb831b527c21c5efd71c4e7a173295b358000
kseyo <kseyo@veepee.com>
2023-07-06 20:28:19 +0200

    update

--- packages/server/routes/studentsRoute.js
+++ packages/server/routes/studentsRoute.js
@@ -29,8 +29,9 @@ router.get("/", async (req, res, next) => {
 });
 
 router.delete('/:deletedValue', async(req,res,next) => {
+  
   try {
-    const data = await studentSchema.deleteOne(req.params.name);
+    const data = await studentSchema.deleteOne({name:req.params.deletedValue});
     console.log("supprime",data)
     res.json(data)
   } catch (error){
@@ -41,29 +42,21 @@ router.delete('/:deletedValue', async(req,res,next) => {
 
 router.put('/:putedValue', async(req,res,next) => {
 
-
-// `doc` is the document _before_ `update` was applied
-// const filter = { name: 'Jean-Luc Picard' };
-// const update = { age: 59 };
-// let doc = await Character.findOneAndUpdate(filter, update);
-// doc.name; // 'Jean-Luc Picard'
-// doc.age; // undefined
-
-// https://mongoosejs.com/docs/tutorials/findoneandupdate.html
+  // console.log("current data",JSON.stringify(req.params,null, 2))
+  
+  // console.log("new data",req.body)
+  
+  const {putedValue} = req.params; // recupere moi putedValue(=value.name) qui est dans req.params envoyé depuis le front
+  // console.log("value",JSON.stringify(putedValue, null, 2))
 
   try {
-    const data = await studentSchema.findOneAndUpdate(req.params.name, { 
+    const data = await studentSchema.findOneAndUpdate({name:putedValue}, { 
       name: req.body.name,
       email: req.body.email
     }, {
       new: true
     });
-
-    console.log(JSON.stringify({ data }, null, 2));
-    console.log(JSON.stringify({ paramsName: req.params.name }, null, 2));
-    console.log(JSON.stringify({ email: req.body.email }, null, 2));
-    console.log(JSON.stringify({ name: req.body.name }, null, 2));
-
+    console.log("update....")
     res.json(data)
   }catch(error){
     return next(error)
@@ -72,3 +65,73 @@ router.put('/:putedValue', async(req,res,next) => {
 
 
 module.exports = router;
+
+
+// router.put('/:putedValue', async(req,res,next) => {
+
+//   try {
+//     const data = await studentSchema.findOneAndUpdate(req.params.name, { 
+//       name: req.body.name,
+//       email: req.body.email
+//     }, {
+//       new: true
+//     });
+//     console.log("update....")
+//     res.json(data)
+//   }catch(error){
+//     return next(error)
+//   }
+// })
+
+
+// router.put('/:putedValue', async(req,res,next) => {
+
+//   try {
+//     const data = await studentSchema.findOneAndUpdate(req.params.email, { 
+//       name: req.body.name,
+//       email: req.body.email
+//     }, {
+//       new: true
+//     });
+//     console.log("update....")
+//     res.json(data)
+//   }catch(error){
+//     return next(error)
+//   }
+// })
+
+
+
+
+/*
+  METHOD HTTP: POST, GET, PUT, DELETE, PATCH...
+
+  POST -- Utiliser lorsqu'on ajoute une ressource ex: Créer un compte utilisateur
+  GET -- Utiliser pour récupérer une ou des ressources ex: Récupérer la liste des utilisateurs de son site
+  PUT -- Utiliser lorsqu'on souhaite modifier entièrement les données d'une ressource ex: Modifier toutes ces données personnelles sur Instagram
+  PATCH --  Utiliser lorsqu'on souhaite modifier partiellement les données d'une ressource ex: Modifier juste son mot de passe utilisateur
+  DELETE -- utiliser pour supprimer une ressource
+
+  Mettons en application le CRUD à partir des méthod HTTP
+  Dans notre exemple, on a une api qui s'appelle https://harvard.com
+
+  Ici, l'url qui concerne tous les élèves de l'école Harvard est la suivante https://harvard.com/students
+
+  --> Liste des élèves de harvard
+  GET https://harvard.com/students
+
+  --> Ajouter un élève dans l'école
+  POST https://harvard.com/students
+
+  --> Récupérer l'élève Killian dans la base de données qui a pour identifiant "ID7458566"
+  GET https://harvard.com/students/ID7458566
+  (dans express router.get https://harvard.com/students/:identifiant)
+
+  --> Supprimer l'élève Killian dans la base de données qui a pour identifiant "ID7458566"
+  DELETE https://harvard.com/students/ID7458566
+
+  --> Modifier l'élève Killian dans la base de données qui a pour identifiant "ID7458566"
+  PUT https://harvard.com/students/${parametre} -> value.name -> killian 
+  (dans express router.put https://harvard.com/students/:identifiant) req.params.identifiant
+
+*/
